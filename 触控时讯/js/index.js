90cdd2272d2ba4c172c834914012d79b82da944c
Your Name <email@example.com>
2017-06-16 11:12:25 +0800

    xm

---
+++ 触控时讯/js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+	//首页上面的点击效果
+	$("#topline").on("tap",function(){
+		$(".home-con").animate({marginLeft:0});
+		$(this).addClass("active").siblings().removeClass("active");
+	});
+	$("#subscribe").on("tap",function(){
+		$(".home-con").animate({marginLeft:"-16rem"});
+		$(this).addClass("active").siblings().removeClass("active");
+	})
+})
